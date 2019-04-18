5e466390d148585df271471f0593519f616608e1
Donna Amos <donnacamos.developer@gmail.com>
2019-04-18 01:32:48 +0000

    modified cli.rb

--- lib/makeup_sales/cli.rb
+++ lib/makeup_sales/cli.rb
@@ -7,12 +7,19 @@ class MakeupSales::CLI
   end 
   
   def list_products 
-    puts
+    puts 
     1. NO7 - Line Correcting Booster Serum - Sale Price $29.39 - Original Price $41.99
     2. RAPIDLASH - Eyelash Enhancing Serum - Sale Price $34.99 - Original Price $49.99
-     
   end 
   
   def menu 
-    puts ""
+    puts "Enter the number of the product you'd like more info on:"
+    input = gets.strip 
+    case input 
+    when "1" 
+    puts "More info on product 1..." 
+    when "2" 
+    puts "More info on product 2..." 
+  end 
+end 
 end 
