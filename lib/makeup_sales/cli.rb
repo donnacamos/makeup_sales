2bf18f79433a0167bb459442af3e06cbe64ef4c3
Donna Amos <donnacamos.developer@gmail.com>
2019-04-18 01:16:23 +0000

    modified cli.rb

--- lib/makeup_sales/cli.rb
+++ lib/makeup_sales/cli.rb
@@ -2,5 +2,17 @@ class MakeupSales::CLI
   
   def call 
     puts "Today's Ulta Beauty Sales:" 
+    list_products 
+    menu 
   end 
+  
+  def list_products 
+    puts
+    1. NO7 - Line Correcting Booster Serum - Sale Price $29.39 - Original Price $41.99
+    2. RAPIDLASH - Eyelash Enhancing Serum - Sale Price $34.99 - Original Price $49.99
+     
+  end 
+  
+  def menu 
+    puts ""
 end 
