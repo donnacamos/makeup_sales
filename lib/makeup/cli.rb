117122a78bd7624f81c60ce3ef70cef9ecb4b179
Donna Amos <donnacamos.developer@gmail.com>
2019-04-19 02:45:59 +0000

    modified cli

--- lib/makeup/cli.rb
+++ lib/makeup/cli.rb
@@ -1,37 +1,37 @@
-class MakeupSales::CLI 
-  
-  def call 
-    puts "Today's Ulta Beauty Sales:" 
-    list_products 
-    menu 
-    goodbye 
-  end 
-  
-  def list_products 
-    puts "Today's Ulta Beauty Products on sale now:"
-    puts 
-    1. NO7 - Line Correcting Booster Serum - Sale Price $29.39 - Original Price $41.99
-    2. RAPIDLASH - Eyelash Enhancing Serum - Sale Price $34.99 - Original Price $49.99
-    end 
-    
-    @sales = MakeupSales::Sale.today 
-  
-  def menu 
-    puts "Enter the number of the product you'd like more info on or type list or type exit:"
-    while input != "exit" 
-    input = gets.strip 
-    case input 
-    when "1" 
-    puts "More info on product 1..." 
-    when "2" 
-    puts "More info on product 2..." 
-    when "list"
-    puts list_products 
-  else
-    puts "Not sure what you want: type list or exit."
-  end 
-end 
+class MakeupSales::CLI
 
-    def goodbye 
-      puts "See you tomorrow for more sales on our Fabulous Girlie Go-tos! Ta ta!"
-end 
+  def call
+    list_products 
+    menu
+    goodbye
+  end
+
+  def list_products 
+    puts "Ulta Beauty's Sales Today:" 
+    @sales = Makesales::Sale.today
+    @sales.each.with_index(1) do |product, i|
+      puts "#{i}. #{product.name} - #{product.price} - #{product.availability}"
+    end
+  end
+
+  def menu
+    input = nil
+    while input != "exit"
+      puts "Enter the number of the product you'd like more info on or type list to see the products again or type exit:"
+      input = gets.strip.downcase
+
+      if input.to_i > 0
+        the_product = @sale[input.to_i-1]
+        puts "#{the_product.name} - #{the_product.price} - #{the_product.availability}"
+      elsif input == "list"
+        list_product
+      else
+        puts "Not sure what you want, type list or exit."
+      end
+    end
+  end
+
+  def goodbye
+    puts "See you tomorrow for more great products from Ulta Beauty!" 
+  end
+end
--- lib/makeup/makeup-sales.rb
+++ lib/makeup/makeup-sales.rb
@@ -1,3 +1,8 @@
+require 'open-uri'
+require 'nokogiri'
+require 'pry'
+
+
 require_relative "./makeup/sales/version" 
 require-relative "./makeup/sale"
 require_relative "./makeup/cli" 
