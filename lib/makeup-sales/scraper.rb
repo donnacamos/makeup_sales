61d40368e7ee77ae4c9979f5f82b0e0786e782f8
Donna Amos <donnacamos.developer@gmail.com>
2019-04-26 05:29:40 +0000

    update scraper

--- lib/makeup-sales/scraper.rb
+++ lib/makeup-sales/scraper.rb
@@ -1,34 +1,20 @@
-class MakeupSales::Sale 
+class MakeupSales::Scraper 
   
-  attr_accessor :brand, :product, :sale_price, :previous_price, :url 
-
-   def self.today 
-   # scrape Ulta Beauty and then return the sale of the product
-   self.scrape_products 
-   end 
-  
-  def self.scrape_products 
-    products = [] 
-    
-    sale << self.scrape_ulta 
-    
-    deals 
+  def get_page 
+    Nokogiri::HTML(open("https://www.ulta.com/promotion/sale")) 
   end 
   
-  def self.scrape_ulta 
-    
-    doc = Nokogiri::HTML(open("https://www.ulta.com/promotion/sale"))
-    
-    sale = self.new 
-    sale.brand = doc.search("h4.prod-title").text.strip 
-    sale.product = doc.search("p.prod-desc").text.strip
-    sale.sale_price = doc.search("span.new-pro-price").text.strip 
-    sale.previous_price = doc.search("span.pro-old-price").text.strip 
-    sale.url = doc.search("a").first.attr("href").strip 
-    sale.availability = true 
-    
-    deal 
+  def scrape_products_index
+      self.get_page.css("option#/promotion/sale?N=1z13uvl&amp;No=0&amp;Nrpp=98")
+  end 
+  
+  def scrape_products 
+    scrape_products_index.each do |p| 
+      MakeupSales::Product.new_from_index_page(r) 
+    end 
+  end 
 end 
-
-end 
+    
+  
+ 
 
