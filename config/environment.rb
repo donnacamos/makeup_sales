3032f2530e4552a0d43a5e6684d0a7470e9b0d02
Donna Amos <donnacamos.developer@gmail.com>
2019-04-27 17:44:18 +0000

    refactor

--- config/environment.rb
+++ config/environment.rb
@@ -3,6 +3,7 @@ require 'nokogiri'
 require 'pry'
 
 
-require_relative "../lib/makeup-sales/version" 
-require-relative "../lib/makeup-sales/cli"
-require_relative "../lib/makeup-sales/scraper" 
+require_relative '../lib/makeup-sales/version' 
+require-relative '../lib/makeup-sales/cli'
+require_relative '../lib/makeup-sales/scraper'
+require_relative '../lib/makeup-sales/product'
--- makeup-sales.gemspec
+++ makeup-sales.gemspec
@@ -1,7 +1,7 @@
 
 lib = File.expand_path("../lib", __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require "makeup/sales/version"
+require "makeup-sales/version"
 
 Gem::Specification.new do |spec|
   spec.name          = "makeup-sales"
