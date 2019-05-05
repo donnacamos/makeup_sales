062853cd35b37a40ad184b998861b9db9a56d71e
Donna Amos <donnacamos.developer@gmail.com>
2019-05-05 23:18:00 +0000

    modified product and scraper

--- lib/makeup-sales/product.rb
+++ lib/makeup-sales/product.rb
@@ -2,53 +2,8 @@ class MakeupSales::Product
   
   attr_accessor :name, :page_number, :brand, :url, :sale_price, :previous_price, :description  
   
-  @@all = [] 
   
-    def self.new_from_index_page(r)
-    self.new(
-      r.css("h2").text,
-      "https://www.ulta.com/promotion/sale#{r.css("a").attribute("href").text}",
-      r.css("h3").text,
-      r.css(".position").text
-      )
-  end
-
-  def initialize(name=nil, url=nil, page_number=nil)
-    @name = name
-    @url = url
-    @page_number = page_number 
-    @@all << self
-  end
-
-  def self.all
-    @@all
-  end
-
-  def self.find(id)
-    self.all[id-1]
-  end
-
-  def brand 
-    @brand ||= doc.css("h4.prod-title").text
+  
    
-  end
-
-  def sale_price
-    @sale_price ||= doc.css("span.pro-new-price").text
-  end
-
-  def previous_price
-    @previous_price ||= doc.css("span.pro-old-price").text
-    
-  end
-
-  def description
-    @description ||= doc.css("p.prod-description").text
-    
-  end
-
-  def doc
-    @doc ||= Nokogiri::HTML(open(self.url))
-  end
   
 end 
--- lib/makeup-sales/scraper.rb
+++ lib/makeup-sales/scraper.rb
@@ -7,12 +7,7 @@ class MakeupSales::Scraper
   def scrape_products_index
       self.get_page.css("option#/promotion/sale?N=1z13uvl&amp;No=0&amp;Nrpp=98")
   end 
-  
-  def scrape_products 
-    scrape_products_index.each do |r| 
-      MakeupSales::Product.new_from_index_page(r) 
-    end 
-  end 
+   
 end 
     
   
