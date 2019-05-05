54f7f3b20a6c080c4772a10199f5af49a6adc775
Donna Amos <donnacamos.developer@gmail.com>
2019-05-05 23:16:00 +0000

    modified cli

--- lib/makeup-sales/cli.rb
+++ lib/makeup-sales/cli.rb
@@ -1,10 +1,16 @@
 class MakeupSales::CLI
 
   def call
-    MakeupSales::Scraper  
+    MakeupSales::Scraper
+    puts "" 
     puts "Hello Beautiful! Welcome to the Makeup Sales of Ulta Beauty:"
-    
+    start 
   end
 
-  
+  def start 
+    puts ""
+    puts "What page number would you like to see first?"
+    puts ""
+    puts "1, 2, 3, 4, 5, 6, 7, or press q to quit"
+  end   
 end 
