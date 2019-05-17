a0ce59406c9187ec0eb097ec09e3fc663443620e
Donna Amos <donnacamos.developer@gmail.com>
2019-05-17 00:09:38 +0000

    modified scraper

--- lib/makeup-sales/scraper.rb
+++ lib/makeup-sales/scraper.rb
@@ -5,12 +5,11 @@ class Scraper
     
     doc = Nokogiri::HTML(open("https://www.ulta.com/promotion/sale"))
     
-     products = section.css("li") 
+    # products = section.css("li") 
     
-    doc.products.css(".productQvContainer").each do |product| 
-      product_object = Product.new 
-      product_object.title = product.css("h4.prod-title").text
-      product_object.brand = product.css("div.prod-title").text
+    doc.products.css("div.product-category-container").each do |product| 
+      title = product.css("h4.prod-title").text
+      brand = product.css("div.prod-title").text
       binding.pry
     #  product_object.sale_price = product.css("span.pro-new-price").text
     #  product_object.previous_price = product.css("span.pro-old-price").text
