058bcb6ee2b6da3492b97b58fee6afd00986ca05
Donna Amos <donnacamos.developer@gmail.com>
2019-05-17 02:04:13 +0000

    modified scraper

--- lib/makeup-sales/scraper.rb
+++ lib/makeup-sales/scraper.rb
@@ -1,7 +1,7 @@
 module MakeupSales
 class Scraper 
   
- # def self.scrape_product 
+  def self.scrape_product 
     
     doc = Nokogiri::HTML(open("https://www.ulta.com/promotion/sale"))
     binding.pry 
@@ -10,12 +10,11 @@ class Scraper
    # doc.css("div.product-category-container").each do |product| 
     #  title = product.css("h4.prod-title").text
      # brand = product.css("div.prod-title").text
-    #  product_object.sale_price = product.css("span.pro-new-price").text
-    #  product_object.previous_price = product.css("span.pro-old-price").text
-    #  product_object.description = product.css("div.ProductDetail_productContent collapse in").text
-    #  product_object.url = "https://www.ulta.com" + product.css("a")[0]('href')
+    #  sale_price = product.css("span.pro-new-price").text
+    #  previous_price = product.css("span.pro-old-price").text
+    #  description = product.css("div.ProductDetail_productContent collapse in").text
   #  end 
- # end 
+  end 
   
   #def self.scrape_product_details(product_object) 
   #  website = Nokogiri::HTML(open(product_object.url)) 
